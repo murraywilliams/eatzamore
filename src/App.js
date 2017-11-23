b8ceb233c7cc07503925ee1960bbac32efa94cbb
Murray Williams <murray.williams@ventureweb.com>
2017-11-23 14:35:45 +0200

    seperated out api values and pushed into food array

--- src/App.js
+++ src/App.js
@@ -7,27 +7,39 @@ class App extends Component {
 
     this.state = {
       foods: [],
+      order: [],
     }
   }
 
   componentDidMount() {
-
+    let newRes;
     fetch('https://murraywilliams.co.za/eatz/wp-json/wp/v2/food')
       .then(res => res.json())
       .then(res => {
-        this.setState({
-          foods: res
+        newRes = res.map((data) => {
+          return ({
+            id: data.id,
+            name: data.title.rendered,
+            desc: data.acf.desc,
+            price: data.acf.price,
+            image: data.acf.image,
+          })
         })
-      });
+        this.setState ({
+          foods: newRes
+        });
+      })
+      console.log(newRes);
   }
 
   render() {
 
     let foods = this.state.foods.map((food, index) => {
       return <div key={index}>
-        <h3>{food.title.rendered}</h3>
-        <p>{food.acf.desc}</p>
-        <img width="120px" src={food.acf.image} />
+        <h3>{food.name}</h3>
+        <p><i>R{food.price}.00</i></p>
+        <p>{food.desc}</p>
+        <img width="120px" src={food.image} />
       </div>
     });
     return (
